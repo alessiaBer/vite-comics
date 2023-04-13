48def5fc0ab66235992a0b5d961081328c122a43
alessiaBer <alexdeyn@gmail.com>
2023-04-13 19:13:06 +0200

    [add] google font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/scss/app.scss'
 import '../node_modules/bootstrap/scss/bootstrap.scss'
 import "@fontsource/roboto-condensed/400.css"
 import "@fontsource/roboto-condensed/700.css"
+import "@fontsource/roboto/700.css"
 
 import App from './App.vue'
 

41839356dd64a2e4594862a02345f818242401f0
alessiaBer <alexdeyn@gmail.com>
2023-04-13 16:35:11 +0200

    [add] main block

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,26 @@ export default {
     <div class="header_container"></div>
   </header>
 
-  <main></main>
+  <main>
+    <div class="main_content">
+      <div class="content_container">
+      --&gt; Content goes here &lt;--
+      </div>
+
+    </div>
+    <div class="shop">
+      <div class="shop_container"></div>
+    </div>
+  </main>
 
   <footer></footer>
 </template>
 
 <style lang="scss" scoped>
+
 header {
   height: 80px;
   background-color: red;
 }
+
 </style>

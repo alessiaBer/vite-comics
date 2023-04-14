34cb57a5040a6eee7f12d827a97ea1a1b0ce9132
alessiaBer <alexdeyn@gmail.com>
2023-04-14 17:48:24 +0200

    [fix] footer top

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -1,77 +1,84 @@
 <script>
 export default {
-    name: 'FooterTop',
-    data() {
+  name: "FooterTop",
+  data() {
     return {
-        menu: [
+      menuLeft: [
         {
-            title: 'DC Comics',
-            items: [
-                'Characters',
-                'Comics',
-                'Movies',
-                'TV',
-                'Games',
-                'Videos',
-                'News'
-            ]
+          title: "DC Comics",
+          items: [
+            "Characters",
+            "Comics",
+            "Movies",
+            "TV",
+            "Games",
+            "Videos",
+            "News",
+          ],
         },
         {
-            title: 'DC',
-            items: [
-                'Terms of Use',
-                'Privacy Policy (New)',
-                'Ad Choice',
-                'Avertising',
-                'Jobs',
-                'Subscriptions',
-                'Talent Workshop',
-                'CPSC Certificates',
-                'Ratings',
-                'Shop Help',
-                'Contact Us'
-            ]
+          title: "Shop",
+          items: ["Shop DC", "Shop DC Collectibles"],
+        },
+      ],
+      menuRight: [
+        {
+          title: "DC",
+          items: [
+            "Terms of Use",
+            "Privacy Policy (New)",
+            "Ad Choice",
+            "Avertising",
+            "Jobs",
+            "Subscriptions",
+            "Talent Workshop",
+            "CPSC Certificates",
+            "Ratings",
+            "Shop Help",
+            "Contact Us",
+          ],
         },
         {
-            title: 'Sites',
-            items: [
-                'DC',
-                'MAD Magazine',
-                'DC Kids',
-                'DC Universe',
-                'DC Power Visa'
-            ]
+          title: "Sites",
+          items: [
+            "DC",
+            "MAD Magazine",
+            "DC Kids",
+            "DC Universe",
+            "DC Power Visa",
+          ],
         },
-        {
-            title: 'Shop',
-            items: [
-                'Shop DC',
-                'Shop DC Collectibles'
-            ] 
-        }
-    ]
+      ],
     };
   },
 };
 </script>
 <template>
   <div class="footer_top">
-      <div class="top_container">
-        <div class="footer_menu">
-            <div class="my_col" v-for="list in menu">
-                <h5>{{ list.title.toUpperCase() }}</h5>
-                <ul class="list-unstyled">
-                    <li v-for="item in list.items">
-                        <a href="#">{{ item }}</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="top_container">
+      <div class="footer_menu">
+        <div class="my_col">
+          <div class="my_row pb-4" v-for="list in menuLeft">
+            <h5>{{ list.title.toUpperCase() }}</h5>
+            <ul class="list-unstyled flex-column">
+              <li v-for="item in list.items">
+                <a href="#">{{ item }}</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="footer_logo">
+        <div class="my_col" v-for="list in menuRight">
+          <h5>{{ list.title.toUpperCase() }}</h5>
+          <ul class="list-unstyled">
+            <li v-for="item in list.items">
+              <a href="#">{{ item }}</a>
+            </li>
+          </ul>
         </div>
-
       </div>
+      <div class="footer_logo"></div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
